180ed5889d4305b4f236274fc47e700e765c5dfd
thecloer <83699438+thecloer@users.noreply.github.com>
2023-09-24 21:38:24 +0900

    Time: 0 ms (100%), Space: 7.1 MB (91.83%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,16 @@
-using namespace std;
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         bool used[256] = {};
-        queue<int> q;
         int ans = 0;
-        for(char c:s){
-            if(used[c]) {
-                while(!q.empty() && q.front() != c){
-                    used[q.front()] = false;
-                    q.pop();
+        for(int left=0, right=0, n=s.length(); right<n; ans=max(ans,++right-left)){
+            int x = s[right];
+            if(used[x]){
+                while(left < right && s[left] != x){
+                    used[s[left++]] = false;
                 }
-                if(!q.empty()) 
-                    q.pop();
-                q.push(c);
-            } else {
-                used[c] = true;
-                q.push(c);
-            }
-
-            if(ans < q.size()) 
-                ans = q.size();
+                if(left < right) left++;
+            } else used[x] = true;
         }
         return ans;
     }
