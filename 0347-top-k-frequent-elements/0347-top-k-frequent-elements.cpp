1ac3fc80b65ec22d3dae92852ca64257b9e071e4
thecloer <83699438+thecloer@users.noreply.github.com>
2024-05-17 17:08:14 +0900

    Time: 12 ms (62.72%), Space: 17 MB (83.96%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,21 @@
+struct entry { int key, value; };
+bool cmp(const entry &a, const entry &b) { return a.value < b.value; }
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> freq;
+        priority_queue<entry, vector<entry>, decltype(&cmp)> pq(cmp);
+        for(int n:nums) freq[n]++;
+        for(const auto &[key, value]:freq)
+            pq.push({ key, value });
+
+        vector<int> ans(k);
+        for(int i=0; i<k; i++) {
+            ans[i] = pq.top().key;
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
