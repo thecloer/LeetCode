0544081003cce97c0ea440a7941e031542a6f2ee
thecloer <83699438+thecloer@users.noreply.github.com>
2024-09-22 16:26:48 +0900

    Time: 120 ms (60.12%), Space: 74.8 MB (34.11%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -15,18 +15,3 @@ public:
         return ans;
     }
 };
-/*
-        if(nums.size() == 0) return 0;
-        unordered_set<int> st(nums.begin(), nums.end());
-
-        int ans = 1;
-        for(int x:nums) {
-            if(st.find(x-1) != st.end()) continue;
-
-            int len = 1;
-            while(st.find(++x) != st.end()) len++;
-
-            ans = max(ans, len);
-        }
-        return ans;
-*/
